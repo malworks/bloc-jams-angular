5033b17306cfdb8e4230a2997f76e53d8311d052
Mallory Robertson <mallory@mal.works>
2016-09-19 17:57:39 -0700

    Checkpoint 3: Assignment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,12 +10,17 @@
         	.state('landing', {
             	url: '/',
             	templateUrl: '/templates/landing.html'
-         })
+         	})
 
-        .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
-         });
+        	.state('album', {
+            	url: '/album',
+            	templateUrl: '/templates/album.html'
+         	})
+
+         	.state('collection', {
+            	url: '/collection',
+            	templateUrl: '/templates/collection.html'
+         	});
     }
  
     angular
