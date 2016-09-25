bad04fd3930c9f0b34ad98e13a03f30b66d7e325
Mallory Robertson <mallory@mal.works>
2016-09-24 22:26:10 -0700

    Checkpoint 7: Services pt 2

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,10 @@
 (function() {
-    function AlbumCtrl(Fixtures) {
-    
+    function AlbumCtrl(Fixtures, SongPlayer) {
         this.albumData = Fixtures.getAlbum();
-     	
-    };
- 
-     angular
-         .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        this.songPlayer = SongPlayer;
+    }
+    
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
  })();
--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -48,7 +48,7 @@
 		};
 
         Fixtures.getAlbum = function() {
-            return albumDuchamp;  
+            return albumPicasso;  
         };
         
         Fixtures.getCollection = function(numberOfAlbums) {
